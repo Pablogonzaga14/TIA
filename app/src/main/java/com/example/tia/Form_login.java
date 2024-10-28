2357d1d4ad3aec0cff8911fe31a392467f3390f9
pablo <pablogonzaga14@gmail.com>
2024-10-28 20:29:54 -0300

    Finalizando a tela de login e iniciando a tela de cadastro- corrrigido

--- app/src/main/java/com/example/tia/Form_login.java
+++ app/src/main/java/com/example/tia/Form_login.java
@@ -23,13 +23,15 @@ public class Form_login extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        IniciarComponentes();
         txt_criar_conta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Form_login.this,Form_Cadastro.class);
+                Intent intent = new Intent(Form_login.this, Form_Cadastro.class);
                 startActivity(intent);
             }
         });
+
     }
     public void IniciarComponentes(){
         txt_criar_conta = findViewById(R.id.txt_criar_conta);
