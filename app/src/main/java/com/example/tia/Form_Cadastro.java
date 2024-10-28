2a6a9ff57174b8641209e75d1605261dc71c7c1b
pablo <pablogonzaga14@gmail.com>
2024-10-28 20:04:16 -0300

    Finalizando a tela de login e iniciando a tela de cadastro

--- app/src/main/java/com/example/tia/Form_Cadastro.java
+++ app/src/main/java/com/example/tia/Form_Cadastro.java
@@ -1,9 +1,6 @@
 package com.example.tia;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,27 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Form_login extends AppCompatActivity {
-    private TextView txt_criar_conta;
+public class Form_Cadastro extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
-        setContentView(R.layout.activity_form_login);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_form_cadastro);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        txt_criar_conta.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Form_login.this,Form_Cadastro.class);
-                startActivity(intent);
-            }
-        });
-    }
-    public void IniciarComponentes(){
-        txt_criar_conta = findViewById(R.id.txt_criar_conta);
     }
 }
