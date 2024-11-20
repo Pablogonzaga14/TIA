74c50e2ff5e32db262cf1421f44c101188b778d9
pablo <pablogonzaga14@gmail.com>
2024-11-20 16:35:31 -0300

    Autenticando Usuarios

--- app/src/main/java/com/example/tia/Form_login.java
+++ app/src/main/java/com/example/tia/Form_login.java
@@ -2,17 +2,35 @@ package com.example.tia;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseNetworkException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+
 public class Form_login extends AppCompatActivity {
-    private TextView txt_criar_conta;
+    private TextView txt_criar_conta,txt_mensagem_erro;
+    private EditText edit_email,edit_senha;
+    private Button bt_entrar;
+    private ProgressBar progressbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,8 +50,65 @@ public class Form_login extends AppCompatActivity {
             }
         });
 
+        bt_entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = edit_email.getText().toString();
+                String senha = edit_senha.getText().toString();
+
+                if (email.isEmpty() || senha.isEmpty()){
+                    txt_mensagem_erro.setText("Preencha todos os campos");
+                }else{
+                    txt_mensagem_erro.setText("");
+                    AutenticarUsuario();
+                }
+            }
+        });
+
+    }
+    public void AutenticarUsuario(){
+        String email = edit_email.getText().toString();
+        String senha = edit_senha.getText().toString();
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    progressbar.setVisibility(View.VISIBLE);
+                    new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            IniciarTelaProdutos();
+                        }
+                    },3000);
+                }else {
+                    String erro;
+                    try {
+                        throw task.getException();
+                    }catch (FirebaseAuthWeakPasswordException e){
+                        erro =  "Coloque uma senha com no minimo 6 caracteres";
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+                        erro = "E-mail inválido";
+                    }catch (FirebaseNetworkException e){
+                        erro = "Sem conexão com a internet";
+                    }catch (Exception e){
+                        erro = "Erro ao Logar usuário";
+                    }
+                    txt_mensagem_erro.setText(erro);
+                }
+            }
+        });
+}
+    public  void IniciarTelaProdutos(){
+        Intent intent = new Intent(Form_login.this, Lista_produtos.class);
+        startActivity(intent);
+        finish();
     }
     public void IniciarComponentes(){
         txt_criar_conta = findViewById(R.id.txt_criar_conta);
+        txt_mensagem_erro = findViewById(R.id.txt_mensagem_erro);
+        edit_email = findViewById(R.id.edit_email);
+        edit_senha = findViewById(R.id.edit_senha);
+        bt_entrar = findViewById(R.id.bt_entrar);
+        progressbar =findViewById(R.id.progressbar);
     }
 }
