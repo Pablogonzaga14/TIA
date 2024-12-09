48d1bf1ca50ceb735165cc737d93957177a025f5
pablo <pablogonzaga14@gmail.com>
2024-12-09 20:31:14 -0300

    Validando a lista de produtos

--- app/src/main/java/com/example/tia/Lista_produtos.java
+++ app/src/main/java/com/example/tia/Lista_produtos.java
@@ -12,11 +12,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.tia.Adapter.AdapterProduto;
+import com.example.tia.Model.Produto;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class Lista_produtos extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Lista_produtos extends AppCompatActivity {
+    private RecyclerView recyclerView_produtos;
+    private AdapterProduto adapterProduto;
+    private List <Produto> produtoList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +35,36 @@ public class Lista_produtos extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        recyclerView_produtos = findViewById(R.id.recycleView_produtos);
+        produtoList = new ArrayList<>();
+        adapterProduto = new AdapterProduto(getApplicationContext(),produtoList);
+        recyclerView_produtos.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView_produtos.setHasFixedSize(true);
+        recyclerView_produtos.setAdapter(adapterProduto);
+
+        Produto produto = new Produto(R.drawable.ic_launcher_background,"produto 1", "R$20");
+        produtoList.add(produto);
+
+        Produto produto1 = new Produto(R.drawable.ic_launcher_background,"produto 1", "R$20");
+        produtoList.add(produto1);
+
+        Produto produto2 = new Produto(R.drawable.ic_launcher_background,"produto 1", "R$20");
+        produtoList.add(produto2);
+
+        Produto produto3 = new Produto(R.drawable.ic_launcher_background,"produto 1", "R$20");
+        produtoList.add(produto3);
+
+        Produto produto4 = new Produto(R.drawable.ic_launcher_background,"produto 1", "R$20");
+        produtoList.add(produto4);
+
+        Produto produto5 = new Produto(R.drawable.ic_launcher_background,"produto 1", "R$20");
+        produtoList.add(produto5);
+
+        Produto produto6 = new Produto(R.drawable.ic_launcher_background,"produto 1", "R$20");
+        produtoList.add(produto6);
+
+        Produto produto7 = new Produto(R.drawable.ic_launcher_background,"produto 1", "R$20");
+        produtoList.add(produto7);
     }
 //metodo responsavel por criar um menu novo para gente usar
     @Override
---
+++ app/src/main/java/com/example/tia/Model/Produto.java
@@ -0,0 +1,47 @@
+package com.example.tia.Model;
+
+public class Produto {
+
+    private int foto;
+    private String nome;
+    private String preco;
+    private String descricao;
+
+    public Produto(int foto, String nome, String preco) {
+        this.foto = foto;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
