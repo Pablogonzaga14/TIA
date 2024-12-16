2d9f9c44c0e4114f88692735595051e95a478345
pablo <pablogonzaga14@gmail.com>
2024-12-16 15:43:31 -0300

    criando lista de produtos e puxando os itens da lista do banco de dados

--- app/src/main/java/com/example/tia/Lista_produtos.java
+++ app/src/main/java/com/example/tia/Lista_produtos.java
@@ -12,11 +12,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.tia.Adapter.AdapterProduto;
+import com.example.tia.Model.Produto;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Lista_produtos extends AppCompatActivity {
-
+    private RecyclerView recyclerViewProdutos;
+    private AdapterProduto adapterProduto;
+    private List <Produto> produtoList;
+    private FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +41,31 @@ public class Lista_produtos extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        recyclerViewProdutos = findViewById(R.id.recyclerViewProdutos);
+        produtoList = new ArrayList<>();
+        adapterProduto = new AdapterProduto(getApplicationContext(),produtoList);
+        recyclerViewProdutos.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerViewProdutos.setHasFixedSize(true);
+        recyclerViewProdutos.setAdapter(adapterProduto);
+        db = FirebaseFirestore.getInstance();
+        db.collection("Produtos").orderBy("nome")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for (QueryDocumentSnapshot queryDocumentSnapshot: task.getResult()){
+                                Produto produto = queryDocumentSnapshot.toObject(Produto.class);
+                                produtoList.add(produto);
+                                adapterProduto.notifyDataSetChanged();
+                            }
+                        }
+                    }
+                });
+
+
+
     }
 //metodo responsavel por criar um menu novo para gente usar
     @Override
